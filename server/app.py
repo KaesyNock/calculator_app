2ffec8382a1239c434025fbf27c091a938d3bae0
ChawkiMbarki <chawkimbarki500@gmail.com>
2024-12-11 16:22:38 +0100

    Added testing configuration

--- server/app.py
+++ server/app.py
@@ -1,11 +1,5 @@
-from flask import Flask
-from app.routes.user_routes import user_bp
-from app.routes.calculation_routes import calculation_bp
-
-app = Flask(__name__)
-
-app.register_blueprint(user_bp, url_prefix='/api/user')
-app.register_blueprint(calculation_bp, url_prefix='/api/math')
+from app import create_app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
