0493f879dd0e3b6fa293393b9439dc65089d0d3c
ChawkiMbarki <chawkimbarki500@gmail.com>
2024-12-11 16:23:45 +0100

    Created middleware and token creation/validation methods

--- server/app/services/user_service.py
+++ server/app/services/user_service.py
@@ -1,23 +1,21 @@
 from app.models import User
+from app.utils import create_token
 
 def register_user_service(username):
     user_data = {'username': username}
-    
-    # add user validation (validate_registration don't exist yet)
-    """ errors = User.validate_registration(user_data)
-    if errors:
-        return False, errors """
-    
-    # this will save user to the database
     User.save(user_data)
-    return True, "User registered successfully!"
+    token = create_token(user_data['username'], user_data['username'])
+    
+    return True, "User registered successfully!", {"username": user_data['username'], "token": token}
 
-def login_user_service(username):
-    login_data = {'login_username': username}
+def login_user_service(user_id):
+    user = User.get_user({"id": user_id})
+    if not user:
+        return False, f"User with the ID {user_id} was not found. Please register first", user_id
 
-    # add login validation (validate_login don't exist yet)
-    """ errors = User.validate_login(login_data)
-    if errors:
-        return False, errors """
-
-    return True, "Logged in successfully!"
+    token = create_token(user.id, user.username)
+    return True, "User logged in successfully!", {
+        "id": user.id,
+        "username": user.username,
+        "token": token
+    }
--- server/app/utils.py
+++ server/app/utils.py
@@ -1,1 +1,28 @@
-# this for encryption method and other stuff
+import jwt
+from datetime import datetime, timezone, timedelta
+
+SECRET_KEY = "bla bla bla" 
+
+# this will decode and verify token
+def decode_and_verify_token(token):
+    try:
+        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        # check token expiration
+        if decoded_token.get("exp") < datetime.now(tz=timezone.utc).timestamp():
+            return False, "Token has expired"
+        return True, decoded_token
+    except jwt.ExpiredSignatureError:
+        return False, "Token has expired"
+    except jwt.InvalidTokenError:
+        return False, "Invalid token"
+
+# this will create a new token
+def create_token(user_id, username):
+    expiration_time = datetime.utcnow() + timedelta(hours=1)
+    payload = {
+        "id": user_id,
+        "username": username,
+        "exp": expiration_time
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    return token
